54c30994348df551ec0d8a324623fce5043a6249
Nic Terreri <niccolo.terreri@gmail.com>
2016-07-16 18:35:57 +0100

    added documentation to train function

--- categorize/training.py
+++ categorize/training.py
@@ -5,6 +5,10 @@ Module responsible for the training of a categorizer.
 import textblob.classifiers
 
 def train(datafilepath="categorize/data/sample_data.csv"):
+    '''
+    Trains a classifier over the data at the filepath.
+    Assumes file format is csv.
+    '''
     with open(datafilepath, 'r') as istream:
         classifier = textblob.classifiers.NaiveBayesClassifier(istream, format="csv")
     return classifier
