0c7c578988f3b00ee503175fd937244b56a08532
Nic Terreri <niccolo.terreri@gmail.com>
2016-07-17 13:28:27 +0100

    implemented dataset reading based in textblob implementation, should perhaps modify source and make pull request?

--- categorize/dataset_reading.py
+++ categorize/dataset_reading.py
@@ -1,20 +1,22 @@
 '''
 Module responsible for reading a dataset from file.
 
-Adapted from: https://github.com/sloria/TextBlob/blob/dev/textblob/classifiers.py#L128
 '''
 from textblob import formats as formats
 
 def read_data(dataset, format=None, **kwargs):
     """Reads a data file and returns an iterable that can be used
     as testing or training data.
+
+    Adapted from:
+    https://github.com/sloria/TextBlob/blob/dev/textblob/classifiers.py#L128
     """
     # Attempt to detect file format if "format" isn't specified
     if not format:
         format_class = formats.detect(dataset)
         if not format_class:
-            raise FormatError('Could not automatically detect format for the given '
-                              'data source.')
+            raise FormatError('Could not automatically detect format for the '
+                              'given data source.')
     else:
         registry = formats.get_registry()
         if format not in registry.keys():
--- categorize/demo.py
+++ categorize/demo.py
@@ -4,34 +4,7 @@ from categorize.classifierSerializer import serialize
 from categorize.classifierDeserializer import deserialize
 from categorize.dataset_reading import read_data
 
-datafilepath="categorize/data/sample_data.csv"
-
 def demo(args):
-    with open(datafilepath, 'r') as istream:
-        labelled_data = read_data(istream, format="csv")
-
-    # pprint.pprint(labelled_data)
-    testsize = int(len(labelled_data) * 0.1)
-    # print "Testsize: {}".format(testsize)
-    devsize = int((len(labelled_data) - testsize) * 0.1 )
-    # print "Devsize: {}".format(devsize)
-    # print "len(labelled_data) - testsize: {}".format(len)
-    # print "testsize + devsize: {}".format(testsize + devsize)
-
-    test_set  = labelled_data[:testsize]
-    dev_set   = labelled_data[testsize:testsize + devsize]
-    train_set = labelled_data[testsize + devsize:]
-
-    print "\n\nTrain set:"
-    pprint.pprint(train_set)
-    print "\n\nDev set:"
-    pprint.pprint(dev_set)
-    print "\n\nTest set:"
-    pprint.pprint(test_set)
-
-    for pair in train_set:
-        assert pair not in dev_set
-        assert pair not in test_set
 
     # classifier = train()
     # serialize(classifier)
