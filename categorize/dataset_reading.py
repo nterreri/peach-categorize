f1673bfef7a23e6a1afecc59ff151c198d0c1828
Nic Terreri <niccolo.terreri@gmail.com>
2016-07-17 12:01:08 +0100

    started working on slicing dataset after loading

---
+++ categorize/dataset_reading.py
@@ -0,0 +1,23 @@
+'''
+Module responsible for reading a dataset from file.
+
+Adapted from: https://github.com/sloria/TextBlob/blob/dev/textblob/classifiers.py#L128
+'''
+from textblob import formats as formats
+
+def read_data(dataset, format=None, **kwargs):
+    """Reads a data file and returns an iterable that can be used
+    as testing or training data.
+    """
+    # Attempt to detect file format if "format" isn't specified
+    if not format:
+        format_class = formats.detect(dataset)
+        if not format_class:
+            raise FormatError('Could not automatically detect format for the given '
+                              'data source.')
+    else:
+        registry = formats.get_registry()
+        if format not in registry.keys():
+            raise ValueError("'{0}' format not supported.".format(format))
+        format_class = registry[format]
+    return format_class(dataset, **kwargs).to_iterable()
